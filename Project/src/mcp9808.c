1c41b5f7c73535bd8d62f98f9976a04bbeb37b8a
mrmdudek <57598064+mrmdudek@users.noreply.github.com>
2019-12-18 12:09:48 +0000

    Add comments

--- Project/src/mcp9808.c
+++ Project/src/mcp9808.c
@@ -4,16 +4,16 @@ void read_resolution(void)
 {
 uint8_t data1;
 
-if (~i2c_start(MCP9808_I2CADDR_DEFAULT<<1)) // if there is device
+if (~i2c_start(MCP9808_I2CADDR_DEFAULT<<1 | 0)) // if there is device, write frame to this device
 {
-    i2c_write(MCP9808_REG_RESOLUTION);
+    i2c_write(MCP9808_REG_RESOLUTION); // write frame with resolution
     i2c_stop();
 
-    i2c_start(MCP9808_I2CADDR_DEFAULT<<1 | 1);
+    i2c_start(MCP9808_I2CADDR_DEFAULT<<1 | 1); // read frame from device, first send address
     data1 = i2c_read_nack();
     i2c_stop();
 
-    // convert and send
+    // convert received data and send via uart
     if ((data1 & 0b11) & 0b11)
     {
         printf("resolution 0.0625\r\n");
@@ -41,21 +41,21 @@ void read_temperature_C(void)
     uint8_t temp_most_sign;
     uint16_t temp_least_sign;
 
-    if (~i2c_start(MCP9808_I2CADDR_DEFAULT<<1)) // if there is device
+    if (~i2c_start(MCP9808_I2CADDR_DEFAULT<<1)) // if there is device, write frame
     {
-        i2c_write(MCP9808_REG_AMBIENT_TEMP);
+        i2c_write(MCP9808_REG_AMBIENT_TEMP); // frame
         i2c_stop();
 
-        i2c_start(MCP9808_I2CADDR_DEFAULT<<1 | 1);
-        data1 = i2c_read_ack();
-        data2 = i2c_read_nack();
+        i2c_start(MCP9808_I2CADDR_DEFAULT<<1 | 1); // read two frames from device
+        data1 = i2c_read_ack(); // first frame with acknowledge
+        data2 = i2c_read_nack(); // second frame without acknowledge
         i2c_stop();
 
-        // convert
+        // convert temperature
         temp_most_sign = ((data1 & 0b1111)<<4) | (data2 & 0b11110000)>>4;
         temp_least_sign = (((data2 & 0b1000)>0)*5000) + (((data2 & 0b100)>0)*2500) + (((data2 & 0b10)>0)*1250) + (((data2 & 0b1)>0)*625);
   
-        // send
+        // send via uart
         printf("temperature = %u.%04u\r\n",temp_most_sign,temp_least_sign);
     }
 }
